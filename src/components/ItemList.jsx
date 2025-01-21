463f212e78eccf0f936c5ee3ee68692c8fa91047
Luan Vendt <luan.vendt@bitize.com.br>
2025-01-21 17:42:08 -0300

    feat: add sorting functionality to ItemList with react-select for better item organization

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+import Select from "react-select";
+import { sortingOptions } from "../lib/constants";
 import EmptyView from "./EmptyView";
 
 export default function ItemList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("default");
+
+  const sortedItems = items.sort((a, b) => {
+    if (sortBy === "packed") return b.packed - a.packed;
+
+    if (sortBy === "unpacked") return a.packed - b.packed;
+
+    return a.id - b.id;
+  });
+
   return (
     <ul className="item-list">
       {items.length === 0 && <EmptyView />}
-      {items.map((item) => (
+
+      {items.length > 0 ? (
+        <section className="sorting">
+          <Select
+            onChange={(option) => setSortBy(option.value)}
+            options={sortingOptions}
+            defaultValue={sortingOptions[0]}
+          />
+        </section>
+      ) : null}
+
+      {sortedItems.map((item) => (
         <Item
           key={item.id}
           item={item}
--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,3 +3,9 @@ export const initialItems = [
   { id: 2, name: "passport", packed: false },
   { id: 3, name: "phone charger", packed: false },
 ];
+
+export const sortingOptions = [
+  { label: "Sort by default", value: "default" },
+  { label: "Sort by packed", value: "packed" },
+  { label: "Sort by unpacked", value: "unpacked" },
+];
