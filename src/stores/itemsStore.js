34e210ba8926db6291037fff7798b361b3a2ffc6
Luan Vendt <luan.vendt@bitize.com.br>
2025-01-22 11:27:28 -0300

    feat: integrate Zustand persistence for item store

--- src/stores/itemsStore.js
+++ src/stores/itemsStore.js
@@ -1,36 +1,46 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { initialItems } from "../lib/constants";
 
-export const useItemsStore = create((set) => ({
-  items: initialItems,
+export const useItemsStore = create(
+  persist(
+    (set) => ({
+      items: initialItems,
 
-  addItem: (name) =>
-    set((state) => ({
-      items: [...state.items, { id: Date.now(), name, packed: false }],
-    })),
+      addItem: (name) =>
+        set((state) => ({
+          items: [...state.items, { id: Date.now(), name, packed: false }],
+        })),
 
-  deleteItem: (id) =>
-    set((state) => ({ items: state.items.filter((item) => item.id !== id) })),
+      deleteItem: (id) =>
+        set((state) => ({
+          items: state.items.filter((item) => item.id !== id),
+        })),
 
-  markAllAsComplete: () =>
-    set((state) => ({
-      items: state.items.map((item) => ({ ...item, packed: true })),
-    })),
+      markAllAsComplete: () =>
+        set((state) => ({
+          items: state.items.map((item) => ({ ...item, packed: true })),
+        })),
 
-  markAllAsIncomplete: () =>
-    set((state) => ({
-      items: state.items.map((item) => ({ ...item, packed: false })),
-    })),
+      markAllAsIncomplete: () =>
+        set((state) => ({
+          items: state.items.map((item) => ({ ...item, packed: false })),
+        })),
 
-  restToInitial: () => set(() => ({ items: initialItems })),
+      restToInitial: () => set(() => ({ items: initialItems })),
 
-  deleteAllItems: () => set(() => ({ items: [] })),
+      deleteAllItems: () => set(() => ({ items: [] })),
 
-  toggleItem: (id) => {
-    set((state) => ({
-      items: state.items.map((item) =>
-        item.id === id ? { ...item, packed: !item.packed } : item
-      ),
-    }));
-  },
-}));
+      toggleItem: (id) => {
+        set((state) => ({
+          items: state.items.map((item) =>
+            item.id === id ? { ...item, packed: !item.packed } : item
+          ),
+        }));
+      },
+    }),
+    {
+      name: "items",
+    }
+  )
+);
